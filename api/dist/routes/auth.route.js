bfdb96c802480afc6bbdebb610f8d0c618f5d74c
Albert <albertjohan2707@gmail.com>
2024-01-03 10:01:40 -0400

    first commit

--- api/dist/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controller/auth.controller");
-const router = (0, express_1.Router)();
-router.post("/auth", auth_controller_1.PostLogin);
-exports.default = router;
